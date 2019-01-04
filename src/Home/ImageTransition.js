13aa4afa36172572f7081ce715d2a3e1e56a34d1
LostDakota <drewmika1@yahoo.com>
2019-01-03 21:45:19 -0500

    blur removal

--- src/Home/ImageTransition.js
+++ src/Home/ImageTransition.js
@@ -1,5 +1,7 @@
 export const ImageTransition = (elementId, image) => {
-    const element = document.getElementById(elementId);
-    element.classList.remove('blur');
-    element.setAttribute('src', image);
+    setTimeout(() => {
+        const element = document.getElementById(elementId);
+        element.classList.remove('blur');
+        element.setAttribute('src', image);
+    }, 300);
 }
--- src/Home/LastEvent.js
+++ src/Home/LastEvent.js
@@ -14,7 +14,7 @@ const LastEvent = props => {
                 <div className="col-12">
                     <div className="card">
                         <div className="blur-mask">
-                            <img id="last-event" className="blur security-images" alt="" src={`${Config.Host}/images/security/last.jpg`} />
+                            <img id="last-event" className="security-images" alt="" src={`${Config.Host}/images/security/last.jpg`} />
                         </div>
                         <p className="t-small t-muted t-right m-5-t">{moment(props.event.time).format('MMM Do YYYY h:mma')}</p>
                     </div>
