5467c67dc610058da265dc5ae95a42609678550c
Drew Mika <drewmika1@yahoo.com>
2019-09-15 14:29:40 -0400

    garage status text

--- src/Controls/Garage.js
+++ src/Controls/Garage.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export const Garage = props => {
     let status;
+    let statusText = 'Closed';
 
     if(props.status){
         status = <i className="fa fa-car garage-status"></i>;
+        statusText = 'Open'
     }
 
     return (
@@ -13,7 +15,7 @@ export const Garage = props => {
                 <i id="door-icon" className="fa fa-warehouse server-icons m-5-y c-lightgray">
                     {status}
                 </i>                
-                <p className="t-small t-muted">Toggle Garage Door</p>
+                <p className="t-small t-muted">`Garage Door is {statusText}`</p>
             </div>            
         </div>
     )
